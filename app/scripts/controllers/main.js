ce375dc4fd4a9e28e432ffb91aff06756d97c62c
余泽江 <yzj1995@vip.qq.com>
2014-08-27 18:28:24 +0800

    主页功能完成

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,16 @@
  * Controller of the posAngularApp
  */
 angular.module('posAngularApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('MainCtrl', function ($scope, $location) {
+      $scope.go_home = function () {
+          $location.path('/');
+      };
+
+      $scope.go_to_list = function () {
+          $location.path('/list');
+      };
+
+      $scope.go_to_cart = function () {
+          $location.path('/cart');
+      };
   });
